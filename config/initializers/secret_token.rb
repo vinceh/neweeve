a18ddacebe6b0ce835e51af85828df346983b678
Vincent Chu <c.tnecniv@gmail.com>
2011-11-01 21:16:33 -0700

    Created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neweeve::Application.config.secret_token = '6b3614ebf3a52c9194871f1cba63477c31eb39dad9870747bce1cdcfc74550077019587e1803c4cd21355caa03e4014cf4bdc69080a33004f44835406a59e609'
